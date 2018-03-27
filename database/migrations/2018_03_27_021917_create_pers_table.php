0e527791ab631c81678418464c52c1fc89759ec3
simocl2017 <simocl@yahoo.fr>
2018-03-26 22:37:49 -0400

    creation des controleurs & modèles & tables de migration. lundi 26/03 a 22h30

---
+++ database/migrations/2018_03_27_021917_create_pers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pers');
+    }
+}
